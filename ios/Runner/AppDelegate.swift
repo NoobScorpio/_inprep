9d690a169481d57a383bbecc7f2ae56464ebc936
noob <noob@noobs-MacBook-Pro.local>
2021-09-15 04:38:29 -0700

    prev

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import Flutter
 import Firebase
 import FirebaseMessaging
+import flutter_downloader
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -12,29 +13,17 @@ import FirebaseMessaging
     if #available(iOS 10.0, *) {
   UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
 }
-//      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//      UNUserNotificationCenter.current().requestAuthorization(
-//    options: authOptions,
-//    completionHandler: {_, _ in })
-//    }
-//    else {
-//  let settings: UIUserNotificationSettings =
-//  UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-//  application.registerUserNotificationSettings(settings)
-//}
+
         GeneratedPluginRegistrant.register(with: self)
+    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
         application.registerForRemoteNotifications()  //* I Forgot This Line *
         
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-//    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-////        Mixpanel.mainInstance().people.addPushDeviceToken(deviceToken) //For Mixpanel purposes, not needed if not using
-//        Messaging.messaging().apnsToken = deviceToken //*THIS WAS MISSING*
-//        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken) //Not sure if calling super is required, but did anyway
-//      }
-//    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Swift.Void) {
-//        Messaging.messaging().appDidReceiveMessage(userInfo)
-//        ///Not sure if calling super is required here
-//        super.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
-//      }
+
+}
+private func registerPlugins(registry: FlutterPluginRegistry) {
+    if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
+       FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
+    }
 }
